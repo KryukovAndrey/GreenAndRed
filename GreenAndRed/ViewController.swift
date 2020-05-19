30f4ef8e4398182230ef40d2f8b0e995c53b619f
KryukovAndrey <kryukov.ios@yandex.ru>
2020-05-19 23:33:04 +0300

    First Commit

---
+++ GreenAndRed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreenAndRed
+//
+//  Created by Andrey on 19.05.2020.
+//  Copyright © 2020 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
